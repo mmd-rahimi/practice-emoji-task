8e51f4c7fe86cf2501d45bee46b3b8c183629f14
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-07-01 15:29:23 -0700

    stored face & locked to local storage

--- src/context/FaceContext.tsx
+++ src/context/FaceContext.tsx
@@ -1,6 +1,12 @@
 "use client";
 import emojiParts from "@/data/emojiParts";
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 export type TFace = {
   eyes: string;
@@ -18,15 +24,18 @@ export type TFaceContext = {
   face: TFace;
   locked: TLock;
   loadFace: () => void;
-  toggleLock: (part: keyof TFace) => void
+  toggleLock: (part: keyof TFace) => void;
 };
 
 const FaceContext = createContext<TFaceContext | undefined>(undefined);
 
 function FaceProvider({ children }: { children: ReactNode }) {
-
   const [face, setFace] = useState<TFace>({ eyes: "", nose: "", mouth: "" });
-  const [locked, setLocked] = useState<TLock>({ eyes: false, nose: false, mouth: false })
+  const [locked, setLocked] = useState<TLock>({
+    eyes: false,
+    nose: false,
+    mouth: false,
+  });
 
   //   pick random part of face
   const GetRandomPart = (part: string[]) => {
@@ -35,20 +44,38 @@ function FaceProvider({ children }: { children: ReactNode }) {
 
   //   loadFace btn
   const loadFace = () => {
-    setFace( (prev) => ({
+    setFace((prev) => ({
       eyes: locked.eyes ? prev.eyes : GetRandomPart(emojiParts.eyes),
       nose: locked.nose ? prev.nose : GetRandomPart(emojiParts.nose),
-      mouth: locked.mouth ? prev.mouth : GetRandomPart(emojiParts.mouth)
+      mouth: locked.mouth ? prev.mouth : GetRandomPart(emojiParts.mouth),
     }));
   };
 
-//   toggle lock
+  //   toggle lock
   const toggleLock = (part: keyof TFace) => {
     setLocked((prev) => ({
-        ...prev,
-        [part]: !prev[part]
-    }))
-  }
+      ...prev,
+      [part]: !prev[part],
+    }));
+  };
+
+//   save states to localStorage
+  useEffect(() => {
+    const storedFace = localStorage.getItem("face");
+    const storedLocked = localStorage.getItem("locked");
+
+    if (storedFace) setFace(JSON.parse(storedFace));
+    if (storedLocked) setLocked(JSON.parse(storedLocked));
+  }, []);
+
+//   save states to localStorage whenever we have change 
+  useEffect(() => {
+    localStorage.setItem("face", JSON.stringify(face))
+  },[face])
+
+    useEffect(() => {
+    localStorage.setItem("locked", JSON.stringify(locked))
+  },[locked])
 
   return (
     <FaceContext.Provider value={{ loadFace, face, locked, toggleLock }}>
